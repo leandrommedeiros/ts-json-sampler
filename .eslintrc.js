5a16dd50a50725ff4d7d5ff0f3f2711bed501f49
Leandro M. Medeiros <leandro.m.medeiros@me.com>
2024-05-07 01:24:45 +0200

    Publish Alpha Version (0.0.1)

---
+++ .eslintrc.js
@@ -0,0 +1,86 @@
+'use strict';
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  plugins: [ 'prettier', 'import', '@typescript-eslint', 'jest-extended' ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
+  env: {
+    browser: false,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'max-classes-per-file': ['error', 1],
+    'no-constructor-return': 'error',
+    'require-await': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        printWidth: 120,
+      },
+    ],
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
+    '@typescript-eslint/class-literal-property-style': ['error', 'getters'],
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/member-delimiter-style': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-invalid-void-type': 'error',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/prefer-enum-initializers': 'error',
+    '@typescript-eslint/prefer-readonly': ['error'],
+    '@typescript-eslint/require-await': 'off',
+    'jest-extended/prefer-to-be-false': 'error',
+    'jest-extended/prefer-to-be-true': 'warn',
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      {
+        accessibility: 'explicit',
+        overrides: {
+          constructors: 'no-public',
+        },
+      },
+    ],
+    '@typescript-eslint/promise-function-async': [
+      'error',
+      {
+        allowedPromiseNames: ['Thenable'],
+        checkArrowFunctions: true,
+        checkFunctionDeclarations: true,
+        checkFunctionExpressions: true,
+        checkMethodDeclarations: true,
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: ['variableLike', 'memberLike'],
+        format: ['camelCase', 'snake_case', 'PascalCase', 'UPPER_CASE'],
+        leadingUnderscore: 'allow',
+      },
+      {
+        selector: ['typeLike'],
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        suffix: ['Interface'],
+      },
+      {
+        selector: ['enumMember'],
+        format: ['UPPER_CASE'],
+      },
+    ],
+  },
+};
